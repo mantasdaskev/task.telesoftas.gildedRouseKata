2bac18ca833e17c2cae6ff000ce802a0473d309e
mantasdaskev <mantas.daskev@gmail.com>
2021-04-22 21:05:55 +0300

    empty project

---
+++ src/GildedRouseKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GildedRouseKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
